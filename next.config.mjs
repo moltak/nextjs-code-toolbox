644eb444a28b46962dad3bdf0e04cd30d0025d6a
KyungHo Jung <moltak@gmail.com>
2024-10-22 20:08:47 +0900

    chore nextjs 15

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export', // Static HTML export
     trailingSlash: true, // 경로에 슬래시를 추가해 정적 파일 접근을 명확히 함
+    reactStrictMode: true,
 };
 
 export default nextConfig;
